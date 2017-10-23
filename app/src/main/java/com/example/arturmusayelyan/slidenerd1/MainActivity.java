760d94a1141f39d187f4ec5bea3629a46ef153c0
artur.musayelyan <musayelyan099@gmail.com>
2017-10-23 18:45:31 +0400

    23.10.2017 ArtCommit ( 1 ) gorcic

--- app/src/main/java/com/example/arturmusayelyan/slidenerd1/MainActivity.java
+++ app/src/main/java/com/example/arturmusayelyan/slidenerd1/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.arturmusayelyan.slidenerd1;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RelativeLayout;
@@ -12,25 +13,35 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     RelativeLayout main;
-    EditText userNameValue, passwordValue;
-    TextView message, userName, password;
-    Button login;
+    EditText etUserNameValue, etPasswordValue;
+    TextView tvMessage, tvUserName, tvPassword;
+    Button btnLogin;
 
-    LayoutParams messageDimensions, userNameDimensions, userNameValueDimensions;
+    LayoutParams messageDimensions, userNameDimensions, userNameValueDimensions, passwordDimensions, passwordValueDimensions, loginDimensions;
 
     int messageId = 1, userNameId = 2, userNameValueId = 3, passwordId = 4, passwordValueId = 5, loginId = 6;
 
+    int paddingValue = 10;
+
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //setContentView(R.layout.activity_main);
         init();
         createMessageTextView();
-        main.addView(message, messageDimensions);
         createUserNameTextView();
-        main.addView(userName, userNameDimensions);
         createUserNameEditText();
-        main.addView(userNameValue,userNameDimensions);
+        createPasswordTextView();
+        createPasswordEditText();
+        createLoginButton();
+
+        main.addView(tvMessage, messageDimensions);
+        main.addView(tvUserName, userNameDimensions);
+        main.addView(etUserNameValue, userNameValueDimensions);
+        main.addView(tvPassword, passwordDimensions);
+        main.addView(etPasswordValue, passwordValueDimensions);
+        main.addView(btnLogin, loginDimensions);
         setContentView(main);
     }
 
@@ -39,35 +50,71 @@ public class MainActivity extends AppCompatActivity {
         LayoutParams mainDimensions = new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
         main.setLayoutParams(mainDimensions);
 
-        userNameValue = new EditText(this);//here comes edit text with user name
-        passwordValue = new EditText(this);
-        message = new TextView(this);//Please login first
-        userName = new TextView(this);// User Name
-        password = new TextView(this);
-        login = new Button(this);
+        etUserNameValue = new EditText(this);//here comes edit text with user name
+        etPasswordValue = new EditText(this);
+        tvMessage = new TextView(this);//Please btnLogin first
+        tvUserName = new TextView(this);// User Name
+        tvPassword = new TextView(this);//tvPassword
+        btnLogin = new Button(this);
     }
 
     private void createMessageTextView() {
         messageDimensions = new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
         messageDimensions.addRule(RelativeLayout.ALIGN_PARENT_LEFT);
-        message.setText("Please Login First");
-        message.setId(messageId);
-        message.setLayoutParams(messageDimensions);
+        tvMessage.setText("Please Login First");
+        tvMessage.setId(messageId);
+
+        tvMessage.setPadding(paddingValue,100,paddingValue,paddingValue);
     }
 
     private void createUserNameTextView() {
         userNameDimensions = new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
-        userName.setText("User Name");
-        userName.setId(userNameId);
+        tvUserName.setText("User Name");
+        tvUserName.setId(userNameId);
         userNameDimensions.addRule(RelativeLayout.ALIGN_PARENT_LEFT);
         userNameDimensions.addRule(RelativeLayout.BELOW, messageId);
+
+        tvUserName.setPadding(paddingValue, paddingValue, paddingValue, paddingValue);
     }
 
     private void createUserNameEditText() {
         userNameValueDimensions = new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
-        userNameValue.setId(userNameValueId);
-        userNameValueDimensions.addRule(RelativeLayout.BELOW,messageId);
-        userNameValueDimensions.addRule(RelativeLayout.RIGHT_OF,userNameId);
+
+        etUserNameValue.setId(userNameValueId);
+        userNameValueDimensions.addRule(RelativeLayout.BELOW, messageId);
+        userNameValueDimensions.addRule(RelativeLayout.RIGHT_OF, userNameId);
         userNameValueDimensions.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
+
+        userNameValueDimensions.addRule(RelativeLayout.ALIGN_BASELINE, userNameId);
+    }
+
+    private void createPasswordTextView() {
+        passwordDimensions = new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+        tvPassword.setText("Password");
+        tvPassword.setId(passwordId);
+        passwordDimensions.addRule(RelativeLayout.ALIGN_PARENT_LEFT);
+        passwordDimensions.addRule(RelativeLayout.BELOW, userNameValueId);
+
+        passwordDimensions.addRule(RelativeLayout.ALIGN_RIGHT, userNameId);
+        tvPassword.setGravity(Gravity.RIGHT);
+        tvPassword.setPadding(paddingValue, paddingValue, paddingValue, paddingValue);
+    }
+
+    private void createPasswordEditText() {
+        passwordValueDimensions = new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+        etPasswordValue.setId(passwordValueId);
+        passwordValueDimensions.addRule(RelativeLayout.RIGHT_OF, passwordId);
+        passwordValueDimensions.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
+        passwordValueDimensions.addRule(RelativeLayout.BELOW, userNameValueId);
+
+        // userNameValueDimensions.addRule(RelativeLayout.ALIGN_BASELINE, passwordId);//? //sxal 62-i verj
+    }
+
+    private void createLoginButton() {
+        loginDimensions = new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+        btnLogin.setId(loginId);
+        btnLogin.setText("Login");
+        loginDimensions.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
+        loginDimensions.addRule(RelativeLayout.BELOW, passwordValueId);
     }
 }
